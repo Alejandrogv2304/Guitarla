722b94220c786703523f8df923161c77c75588c0
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2024-12-18 18:25:18 -0500

    Agregando el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Guitar from './components/guitar'
+import { db } from './data/db';
+
+
+function App() {
+  
+  const [data, setData]= useState(db)
+  const [cart, setCart]= useState([])
+
+  function addToCart(item){
+
+    const itemExits= cart.findIndex(guitar=>guitar.id===item.id)
+    if(itemExits >= 0){
+      const updateCart = [...cart]
+      updateCart[itemExits].quantity++
+      setCart(updateCart)
+    }else{
+      item.quantity = 1
+      setCart([...cart, item])
+    }
+   
+}
+
+  return (
+    <>
+     <Header cart={cart}/>
+    
+     <main className="container-xl mt-5">
+     <h2 className="text-center">Nuestra Colección</h2> 
+    <div className="row mt-5">
+           {/* Así iteramos desde nuestra array para crear divs. */}
+
+           {data.map((guitar)=>(
+            
+           <Guitar
+           key={guitar.id}
+           guitar={guitar}
+           setCart={setCart}
+           cart={cart}
+           addToCart={addToCart}
+           />
+            ))}
+
+        </div>
+    </main>
+
+
+    <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+            <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+    </footer>
+
+    </>
+  )
+}
+
+export default App
